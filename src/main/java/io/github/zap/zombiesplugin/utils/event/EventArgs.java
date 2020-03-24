01a21d5c007cf51409e01303ed6f795a916a1b4a
Tachibana Yui <nguyenminhquan25082003@gmail.com>
2020-03-24 17:10:51 +0700

    Implement/import Simple Event

---
+++ src/main/java/io/github/zap/zombiesplugin/utils/event/EventArgs.java
@@ -0,0 +1,14 @@
+package io.github.zap.zombiesplugin.utils.event;
+
+/**
+ * Represents the base class for classes that contain event data, and provides a
+ * value to use for events that do not include event data.
+ */
+public class EventArgs {
+    private EventArgs() {}
+
+    /**
+     * Provides a value to use with events that do not have event data.
+     */
+    public static EventArgs Empty = new EventArgs();
+}
---
+++ src/main/java/io/github/zap/zombiesplugin/utils/event/EventHandler.java
@@ -0,0 +1,47 @@
+package io.github.zap.zombiesplugin.utils.event;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * encapsulate IEventDelegate
+ * @param <E> Event data type
+ */
+public class EventHandler <E extends EventArgs> {
+    private List<IEventDelegate<E>> subscribers = new ArrayList<>();
+
+    /**
+     * Register an event delegate.
+     * @param delegate the event delegate to register
+     */
+    public void registerEvent(IEventDelegate<E> delegate) {
+        subscribers.add(delegate);
+    }
+
+    /**
+     * Unregister an event delegate.
+     * @param delegate the event delegate to register
+     */
+    public void unregisterEvent(IEventDelegate<E> delegate) {
+        subscribers.remove(delegate);
+    }
+
+    /**
+     * Invoke to all event delegate that register this event hander
+     * @param sender The event sender
+     * @param args Event argument
+     */
+    public void invoke(Object sender, E args) {
+        for (IEventDelegate<E> d : subscribers) {
+            d.execute(sender, args);
+        }
+    }
+
+    /**
+     * Return a list of event delegates that registers this event handler
+     * @return A list of registered event delegate
+     */
+    public List<IEventDelegate<E>> getRegisteredEvent() {
+        return subscribers;
+    }
+}
